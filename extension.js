56e1f22b1a6adb80ddc64d95f46745c9e33a4cd8
Andrey Sitnik <andrey@sitnik.ru>
2023-08-10 23:43:57 +0200

    Update ESLint

--- extension.js
+++ extension.js
@@ -1,58 +1,33 @@
+import GLib from 'gi://GLib'
+import UPower from 'gi://UPowerGlib'
 import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js'
 import { panel } from 'resource:///org/gnome/shell/ui/main.js'
-import UPower from 'gi://UPowerGlib'
-import GLib from 'gi://GLib'
 
 export default class AutohideBatteryExtension extends Extension {
   batteryWatching = null
-
-  settingsWatching = null
 
   initTimeout = null
 
   settings = null
 
-  getBattery(callback) {
-    if (panel.statusArea.quickSettings) {
-      let system = panel.statusArea.quickSettings._system
-      if (system._systemItem._powerToggle) {
-        callback(system._systemItem._powerToggle._proxy, system)
-      }
-    } else {
-      let menu = panel.statusArea.aggregateMenu
-      if (menu && menu._power) {
-        callback(menu._power._proxy, menu._power)
-      }
+  settingsWatching = null
+
+  disable() {
+    if (this.settings) {
+      this.settings.disconnect(this.settingsWatching)
+      this.settings = null
     }
-  }
 
-  show() {
-    this.getBattery((proxy, icon) => {
-      icon.show()
-    })
-  }
+    if (this.initTimeout) {
+      GLib.Source.remove(this.initTimeout)
+      this.initTimeout = null
+    }
 
-  hide() {
-    this.getBattery((proxy, icon) => {
-      icon.hide()
-    })
-  }
-
-  update() {
-    let hideOn = this.settings.get_int('hide-on')
     this.getBattery(proxy => {
-      let isDischarging = proxy.State === UPower.DeviceState.DISCHARGING
-      let isFullyCharged = proxy.State === UPower.DeviceState.FULLY_CHARGED
-      if (proxy.Type !== UPower.DeviceKind.BATTERY) {
-        this.show()
-      } else if (isFullyCharged) {
-        this.hide()
-      } else if (proxy.Percentage >= hideOn && !isDischarging) {
-        this.hide()
-      } else {
-        this.show()
-      }
+      proxy.disconnect(this.batteryWatching)
     })
+
+    this.show()
   }
 
   enable() {
@@ -78,21 +53,46 @@ export default class AutohideBatteryExtension extends Extension {
     )
   }
 
-  disable() {
-    if (this.settings) {
-      this.settings.disconnect(this.settingsWatching)
-      this.settings = null
+  getBattery(callback) {
+    if (panel.statusArea.quickSettings) {
+      let system = panel.statusArea.quickSettings._system
+      if (system._systemItem._powerToggle) {
+        callback(system._systemItem._powerToggle._proxy, system)
+      }
+    } else {
+      let menu = panel.statusArea.aggregateMenu
+      if (menu && menu._power) {
+        callback(menu._power._proxy, menu._power)
+      }
     }
+  }
 
-    if (this.initTimeout) {
-      GLib.Source.remove(this.initTimeout)
-      this.initTimeout = null
-    }
-
-    this.getBattery(proxy => {
-      proxy.disconnect(this.batteryWatching)
+  hide() {
+    this.getBattery((proxy, icon) => {
+      icon.hide()
     })
+  }
 
-    this.show()
+  show() {
+    this.getBattery((proxy, icon) => {
+      icon.show()
+    })
+  }
+
+  update() {
+    let hideOn = this.settings.get_int('hide-on')
+    this.getBattery(proxy => {
+      let isDischarging = proxy.State === UPower.DeviceState.DISCHARGING
+      let isFullyCharged = proxy.State === UPower.DeviceState.FULLY_CHARGED
+      if (proxy.Type !== UPower.DeviceKind.BATTERY) {
+        this.show()
+      } else if (isFullyCharged) {
+        this.hide()
+      } else if (proxy.Percentage >= hideOn && !isDischarging) {
+        this.hide()
+      } else {
+        this.show()
+      }
+    })
   }
 }
--- prefs.js
+++ prefs.js
@@ -1,26 +1,26 @@
-import {
-  ExtensionPreferences,
-  gettext as _
-} from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js'
 import Gtk from 'gi://Gtk'
+import {
+  gettext as _,
+  ExtensionPreferences
+} from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js'
 
 export default class AutohideBatteryPreferences extends ExtensionPreferences {
   fillPreferencesWindow() {
     let settings = this.getSettings()
 
     let grid = new Gtk.Grid({
-      margin_top: 24,
-      margin_bottom: 24,
-      margin_start: 24,
-      margin_end: 24,
       column_spacing: 24,
-      row_spacing: 12,
-      halign: Gtk.Align.CENTER
+      halign: Gtk.Align.CENTER,
+      margin_bottom: 24,
+      margin_end: 24,
+      margin_start: 24,
+      margin_top: 24,
+      row_spacing: 12
     })
 
     let label = new Gtk.Label({
-      label: _('Hide on battery level above'),
-      halign: Gtk.Align.START
+      halign: Gtk.Align.START,
+      label: _('Hide on battery level above')
     })
     grid.attach(label, 0, 0, 1, 1)
 
@@ -31,8 +31,8 @@ export default class AutohideBatteryPreferences extends ExtensionPreferences {
     grid.attach(field, 1, 0, 1, 1)
 
     let note = new Gtk.Label({
-      label: _('If you changed maximum charging level to extend battery life'),
-      halign: Gtk.Align.CENTER
+      halign: Gtk.Align.CENTER,
+      label: _('If you changed maximum charging level to extend battery life')
     })
     note.get_style_context().add_class('dim-label')
     grid.attach(note, 0, 1, 2, 1)
