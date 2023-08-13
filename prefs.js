97ae52a4a1acf4f808edf1f4fe9cf3648c9c4bbd
Andrey Sitnik <andrey@sitnik.ru>
2023-08-13 15:57:40 +0200

    Move settings to Adwaita

--- prefs.js
+++ prefs.js
@@ -1,3 +1,4 @@
+import Adw from 'gi://Adw'
 import Gtk from 'gi://Gtk'
 import {
   gettext as _,
@@ -5,45 +6,35 @@ import {
 } from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js'
 
 export default class AutohideBatteryPreferences extends ExtensionPreferences {
-  fillPreferencesWindow() {
+  fillPreferencesWindow(window) {
     let settings = this.getSettings()
+    window._settings = settings
 
-    let grid = new Gtk.Grid({
-      column_spacing: 24,
-      halign: Gtk.Align.CENTER,
-      margin_bottom: 24,
-      margin_end: 24,
-      margin_start: 24,
-      margin_top: 24,
-      row_spacing: 12
+    let page = new Adw.PreferencesPage()
+    window.add(page)
+
+    let group = new Adw.PreferencesGroup()
+    page.add(group)
+
+    let row = new Adw.SpinRow({
+      adjustment: new Gtk.Adjustment({
+        'lower': 0,
+        'step-increment': 1,
+        'upper': 100,
+        'value': settings.get_int('hide-on')
+      }),
+      subtitle: _(
+        'If you changed maximum charging level to extend battery life'
+      ),
+      title: _('Hide on battery level above')
     })
+    group.add(row)
 
-    let label = new Gtk.Label({
-      halign: Gtk.Align.START,
-      label: _('Hide on battery level above')
-    })
-    grid.attach(label, 0, 0, 1, 1)
-
-    let field = new Gtk.SpinButton()
-    field.set_range(0, 100)
-    field.set_sensitive(true)
-    field.set_increments(1, 10)
-    grid.attach(field, 1, 0, 1, 1)
-
-    let note = new Gtk.Label({
-      halign: Gtk.Align.CENTER,
-      label: _('If you changed maximum charging level to extend battery life')
-    })
-    note.get_style_context().add_class('dim-label')
-    grid.attach(note, 0, 1, 2, 1)
-
-    field.set_value(settings.get_int('hide-on'))
-    field.connect('value-changed', widget => {
-      settings.set_int('hide-on', widget.get_value_as_int())
+    row.connect('changed', () => {
+      settings.set_int('hide-on', row.get_value())
     })
     settings.connect('changed::hide-on', () => {
-      field.set_value(settings.get_int('hide-on'))
+      row.set_value(settings.get_int('hide-on'))
     })
-    return grid
   }
 }
